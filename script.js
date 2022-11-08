e085729612893ccc9434dcd79224e62672c4a9b1
Alin Cola <colaalincristian@gmail.com>
2022-11-08 16:40:41 +0200

    gradient, js optimizing

--- script.js
+++ script.js
@@ -1,14 +1,40 @@
-//let main = document.getElementById("counter")
-//let btn = document.getElementById("reset")
+const counterArea = document.querySelector("#counter");
 
 let count = 0;
 
-counter.onclick = function () {
-    count++ ;
-    counter.innerHTML = count;
+counterArea.innerText = count;
+
+function render() {
+    counterArea.innerText = count;
 }
 
-reset.onclick = function () {
-    count = 0;
-    counter.innerHTML = count;
+let colorCount = 0;
+
+function countUp() {
+    count++ ;
+    render();
+    colorCount++;
+    if (colorCount === 101) {
+        colorCount = 1;
+    }
+    counterArea.style.setProperty("--count", colorCount + "%");
 }
+
+function reset() {
+    count = 0;
+    render();
+    counterArea.style.setProperty("--count", 0 + "%")
+}
+
+counterArea.addEventListener("click", countUp)
+
+document.addEventListener("keydown", function (event) {
+    if (event.code === "Enter" || event.code === "Space")
+        countUp();
+});
+
+const resetBtn = document.querySelector("#reset");
+resetBtn.addEventListener("click", function () {
+    reset();
+    resetBtn.blur();
+});
